3baf38297191ddb9f77b911881306bc6594c8d2b
dineshtiwari69 <210343@softwarica.edu.np>
2023-02-16 16:01:05 +0545

    Support Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: "experimental-edge",
+  },
   async rewrites() {
     return [
       {
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+export const config = {
+  runtime: "experimental-edge",
+};
 export default function handler(req, res) {
   //Bypass Cors Policy using 
 
